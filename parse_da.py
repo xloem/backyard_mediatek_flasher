4f2ed480498d2090d28b2905a899b691e1f9e73c
John Doe <johndoe@example.com>
2022-05-05 09:00:56 -0400

    -

--- parse_da.py
+++ parse_da.py
@@ -26,24 +26,11 @@ with open('MTK_AllInOne_DA.bin', 'rb') as dabin:
 
         # these look to be mostly offsets and lengths for each record and its parts
 
-        # da da 73 81 00 00 00 ca  00 00 00 00 00 10 00 00 
-        dada, hw_code, hw_subcode, hw_ver, sw_ver, unk5 = read('<HHHHLL')
-        print(f'dada={hex(dada)} hw_code={hex(hw_code)} hw_subcode={hex(hw_subcode)} hw_ver={hex(hw_ver)} sw_ver={hex(sw_ver)} unk5={hex(unk5)}')
-        # 00 00 03 00 44 1a 10 00  70 02 00 00 00 00 21 00 
-        unk6, offset1, unk7, unk8 = read('<LLLL')
-        print(f'unk6={hex(unk6)} offset1={hex(offset1)} unk7={hex(unk7)} unk8={hex(unk8)}')
-        # 00 00 00 00 00 00 00 00  b4 1c 10 00 d0 76 01 00 
-        unk9, unk10, offset2, unk12 = read('<LLLL')
-        print(f'unk9={hex(unk9)} unk10={hex(unk10)} offset2={hex(offset2)} unk12={hex(unk12)}')
-        # 00 00 0c 00 d0 75 01 00  00 01 00 00 84 93 11 00 
-        unk13, unk14, unk15, unk16 = read('<LLLL')
-        print(f'unk13={hex(unk13)} unk14={hex(unk14)} unk15={hex(unk15)} unk16={hex(unk16)}')
-        # 30 71 03 00 00 00 00 40  30 70 03 00 00 01 00 00 
-        unk17, unk18, unk19, unk20 = read('<LLLL')
-        print(f'unk17={hex(unk17)} unk18={hex(unk18)} unk19={hex(unk19)} unk20={hex(unk20)}')
-        # 00000350  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
-        # *
-        # 000003d0  00 00 00 00 00 00 00 00  00 00 00 00
-        zeros = dabin.read(0xdc-0x50)
-        zeros_trimmed = zeros.rstrip(b'\0')
-        print(f'zeros={zeros_trimmed}')
+        print(f'index={hex(idx)}')
+
+        dada, hw_code, hw_subcode, hw_ver, sw_ver, unk5, unk6 = read('<HHHHLLL') # 0xdada, 0x7381, 0x00, 0xca00, 0x00, 0x1000, 0x30000
+        print(f'dada={hex(dada)} hw_code={hex(hw_code)} hw_subcode={hex(hw_subcode)} hw_ver={hex(hw_ver)} sw_ver={hex(sw_ver)} unk5={hex(unk5)} unk6={hex(unk6)}')
+        for section in range(10):
+            off, length, unk, startlen, endlen = read('<LLLLL')
+            if off != 0 or length != 0:
+                print(f' section {section}: off={hex(off)} length={hex(length)} unk={hex(unk)} startlen={hex(startlen)} endlen={hex(endlen)}')
